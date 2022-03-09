e4e86a119b02053aa4b5891c65980d5694d34e20
aviva <avivaayasso@icloud.com>
2022-03-09 22:55:21 +0200

    Exercises 1 and 3

---
+++ while_exercises.py
@@ -0,0 +1,14 @@
+#1
+num = 1
+while num <= 20:
+    if num % 2 != 0:
+     print(num)
+    num = num + 1
+
+#3
+number = int(input("enter a number"))
+while number <= 20:
+    if number % 2 != 0:
+        print(number)
+    number = number + 1
+

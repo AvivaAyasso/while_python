121eb931f75736329bedbeaa915c207221545253
aviva <avivaayasso@icloud.com>
2022-03-31 00:13:25 +0300

    3w-Exercises..

--- 3w - while exercises.py
+++ 3w - while exercises.py
@@ -17,3 +17,17 @@ while True:
 
 for l in lines:
     print(l)
+
+#12
+#Find the negative number and change the numbers next to it to negative
+l = [1, 8, 12, 48, -3, 14, 22, 15]
+i = 0
+while i < len(l):
+    if l [i] < 0:
+        if i > 0:
+            l[i-1] *=-1
+        if i < len(l)-1:
+            l[i+1]*=-1
+        i += 1
+    i +=1
+print(l)
